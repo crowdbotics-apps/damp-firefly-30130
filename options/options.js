06292ec2d52a87105e8e8b1b1c7a0c8a3de64735
Crowdbotics <team@crowdbotics.com>
2021-08-26 16:01:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "damp_firefly_30130Identifier",
+  url: "https://damp_firefly_30130Identifier.botics.co",
+  api: "https://damp_firefly_30130Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
